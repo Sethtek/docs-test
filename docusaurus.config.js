8ac15802dc825747a1851aca3078fd4f9dcbe7d8
Sethtek Dev <sethtekdev@gmail.com>
2023-02-25 11:32:50 -0800

    url change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sethtek.github.io',
+  url: 'https://docs.sethtek.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
